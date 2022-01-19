d025d4dab9eb5bc10217b8c0a244b915856dc00c
David Thigpen <davidthigs@gmail.com>
2022-01-18 20:09:35 -0700

    Increase verbosity of tests

--- .github/scripts/test_server.py
+++ .github/scripts/test_server.py
@@ -7,6 +7,7 @@ from pathlib import Path
 def test_in_game_unit_test(rcon_client, mcfunction):
     print(f'Testing: /function {mcfunction}')
     resp = rcon_client.command(f'function {mcfunction}')
+    print(f'Response: {resp}')
     assert len(resp) > 0
     resp = rcon_client.command('scoreboard players get $passed unittest')
     assert '$passed has 1' in resp
