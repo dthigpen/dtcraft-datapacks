0e0dc8bbe038eb0234d4c218934dfee6adb0a5e3
David Thigpen <davidthigs@gmail.com>
2022-01-18 20:19:55 -0700

    Update unit test runner

--- .github/scripts/test_server.py
+++ .github/scripts/test_server.py
@@ -1,6 +1,4 @@
-import os
-import sys
-import pytest
+import time
 from pathlib import Path
 
 
@@ -9,5 +7,6 @@ def test_in_game_unit_test(rcon_client, mcfunction):
     resp = rcon_client.command(f'function {mcfunction}')
     print(f'Response: {resp}')
     assert len(resp) > 0
+    time.sleep(1)
     resp = rcon_client.command('scoreboard players get $passed unittest')
     assert '$passed has 1' in resp
